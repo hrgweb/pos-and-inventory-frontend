5e8594d5fd2bb1fcfdfecd12683d065b6aef4157
Hergen Roque Gamayon <gamayonhergenroque.web@gmail.com>
2023-11-20 00:24:10 +0800

    setup primevue and page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  modules: [
+    'nuxt-primevue'
+  ],
+  primevue: {
+    usePrimeVue: true,
+    options: {
+      ripple: true
+    },
+    components: {
+      include: ['Button']
+    }
+  },
+  css: ['primevue/resources/themes/lara-light-teal/theme.css'],
 })

21cfee02b6a8d52aabb4da42382b8e9f696018e1
Hergen Roque Gamayon <gamayonhergenroque.web@gmail.com>
2023-12-20 02:26:18 +0800

    clean up

--- pages/suppliers.vue
+++ pages/suppliers.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div class="flex align-items-center justify-content-between">
-      <h3>Inventory</h3>
+      <h3>Suppliers</h3>
       <div class="actions pb-3">
         <Button
           label="New Product"
@@ -11,7 +11,7 @@
       </div>
     </div>
 
-    <DataTable :value="products" tableStyle="min-width: 50rem">
+    <DataTable :value="suppliers" tableStyle="min-width: 50rem">
       <template #header>
         <span class="p-input-icon-left">
           <i class="pi pi-search" />
@@ -20,13 +20,6 @@
       </template>
       <Column field="barcode" header="Barcode"></Column>
       <Column field="name" header="Name"></Column>
-      <Column field="description" header="Description"></Column>
-      <!-- <Column field="brand_id" header="Brand"></Column> -->
-      <!-- <Column field="supplier_id" header="Supplier"></Column> -->
-      <Column field="cost_price" header="Cost Price"></Column>
-      <Column field="selling_price" header="Selling Price"></Column>
-      <Column field="stock_qty" header="Stock Qty"></Column>
-      <Column field="reorder_level" header="Reorder Level"></Column>
       <Column field="is_available" header="Available"></Column>
       <Column header="Created">
         <template #body="slotProps">
@@ -56,91 +49,12 @@
       <form method="POST" @submit.prevent="store">
         <div class="flex flex-column gap-2">
           <label for="name">Product Name</label>
-          <InputText id="name" v-model="form.product.name" />
+          <InputText id="name" v-model="form.name" />
         </div>
         <br />
         <div class="flex flex-column gap-2">
           <label for="desc">Product Description</label>
-          <InputText id="desc" v-model="form.product.description" />
-        </div>
-        <br />
-        <!-- <div class="flex flex-column gap-2">
-          <label for="category">Category Id</label>
-          <InputText id="category" v-model.number="form.product.category_id" />
-        </div>
-        <br /> -->
-        <!-- <div class="flex flex-column gap-2">
-          <label for="brand">Brand Id</label>
-          <InputText id="brand" v-model.number="form.product.brand_id" />
-        </div>
-        <br /> -->
-        <div class="flex flex-column gap-2">
-          <label for="supplier">Supplier Id</label>
-          <Dropdown
-            v-model="form.product.supplier_id"
-            :options="transactionTypes"
-            optionLabel="label"
-            optionValue="value"
-            placeholder="Select a City"
-            class="w-full"
-          />
-        </div>
-        <br />
-        <div class="flex flex-column gap-2">
-          <label for="cost">Cost Price</label>
-          <InputText id="cost" v-model.number="form.product.cost_price" />
-        </div>
-        <br />
-        <div class="flex flex-column gap-2">
-          <label for="selling">Selling Price</label>
-          <InputText id="selling" v-model.number="form.product.selling_price" />
-        </div>
-        <br />
-        <div class="flex flex-column gap-2">
-          <label for="stock">Stock Qty</label>
-          <InputText id="stock" v-model.number="form.product.stock_qty" />
-        </div>
-        <br />
-        <div class="flex flex-column gap-2">
-          <label for="reorder">Reorder Level</label>
-          <InputText id="reorder" v-model.number="form.product.reorder_level" />
-        </div>
-        <br />
-        <div class="flex flex-column gap-2">
-          <label for="barcode">Barcode</label>
-          <InputText id="barcode" v-model="form.product.barcode" />
-        </div>
-        <br />
-        <div class="flex flex-column gap-2">
-          <label for="transaction_type">Transaction Type</label>
-          <Dropdown
-            v-model="form.transaction_type"
-            :options="transactionTypes"
-            optionLabel="label"
-            optionValue="value"
-            placeholder="Select a City"
-            class="w-full"
-          />
-        </div>
-        <br />
-        <div class="flex flex-column gap-2">
-          <label for="qty">Qty Change</label>
-          <InputText id="qty" v-model.number="form.qty_change" />
-        </div>
-        <br />
-        <div class="flex flex-column gap-2">
-          <label for="unit">Unit Cost</label>
-          <InputText id="unit" v-model.number="form.unit_cost" />
-        </div>
-        <br />
-        <div class="flex flex-column gap-2">
-          <label for="total">Total Cost</label>
-          <InputText id="total" v-model.number="form.total_cost" />
-        </div>
-        <br />
-        <div class="flex flex-column gap-2">
-          <label for="notes">Notes</label>
-          <InputText id="notes" v-model="form.notes" />
+          <InputText id="desc" v-model="form.description" />
         </div>
         <br />
 
@@ -153,66 +67,25 @@
 <script lang="ts" setup>
 import dayjs from 'dayjs'
 import type { PageState } from 'primevue/paginator'
-import { transactionTypes } from '@/data/transactionTypes'
 
-interface Product {
+interface Supplier {
   name: string
   description: string
-  // category_id: number
-  // brand_id: number
-  supplier_id: number
-  cost_price: number
-  selling_price: number
-  stock_qty: number
-  reorder_level: number
-  barcode: string
-  created_at?: Date
-}
-
-interface Inventory {
-  transaction_type: TransactionType
-  qty_change: number
-  unit_cost: number
-  total_cost: number
-  notes: string
-  product: Product
 }
 
 type Pagination = {
-  data: Product[]
+  data: Supplier[]
   meta: { total: number }
   links: []
 }
 
-enum TransactionType {
-  PURCHASE = 'purchase',
-  SALE = 'sale',
-  ADJUSTMENT = 'adjustment'
-}
-
 const search = ref('')
-const inventory = ref<Inventory[]>([])
-const products = ref<Product[]>([])
+const suppliers = ref<Supplier[]>([])
 const showDialog = ref(false)
 const isLoading = ref(false)
-let form = reactive<Inventory>({
-  transaction_type: TransactionType.PURCHASE,
-  qty_change: 0,
-  unit_cost: 0,
-  total_cost: 0,
-  notes: '',
-  product: {
-    name: '',
-    description: '',
-    // category_id: 0,
-    // brand_id: 0,
-    supplier_id: 0,
-    cost_price: 0,
-    selling_price: 0,
-    stock_qty: 0,
-    reorder_level: 0,
-    barcode: ''
-  }
+let form = reactive<Supplier>({
+  name: '',
+  description: ''
 })
 const pagination = ref<Pagination>({
   data: [],
@@ -230,17 +103,14 @@ async function fetch(): Promise<void> {
 
   try {
     const paginate = (await $fetch(
-      `${config?.public?.backendUrl}/api/inventory/products`,
+      `${config?.public?.backendUrl}/api/inventory/suppliers`,
       { query: { page: curPage.value } }
     )) as Pagination
 
-    if (inventory) {
+    if (paginate?.data?.length) {
       isLoading.value = false
       pagination.value = paginate
-
-      if (paginate?.data?.length) {
-        products.value = paginate?.data
-      }
+      suppliers.value = paginate?.data
     }
   } catch (error) {
     isLoading.value = false
@@ -252,17 +122,17 @@ async function store(): Promise<void> {
   isLoading.value = true
 
   try {
-    const inventory = (await $fetch(
+    const supplier = (await $fetch(
       `${config?.public?.backendUrl}/api/inventory-transactions`,
       {
         method: 'POST',
         body: form
       }
-    )) as Inventory
+    )) as Supplier
 
-    if (inventory) {
+    if (supplier) {
       isLoading.value = false
-      products.value?.push(inventory?.product)
+      suppliers.value?.push(supplier)
       showDialog.value = false
       reset()
     }
@@ -273,21 +143,8 @@ async function store(): Promise<void> {
 }
 
 function reset(): void {
-  form.transaction_type = TransactionType.PURCHASE
-  form.qty_change = 0
-  form.unit_cost = 0
-  form.total_cost = 0
-  form.notes = ''
-  form.product.name = ''
-  form.product.description = ''
-  // form.product.category_id = 0
-  // form.product.brand_id = 0
-  form.product.supplier_id = 0
-  form.product.cost_price = 0
-  form.product.selling_price = 0
-  form.product.stock_qty = 0
-  form.product.reorder_level = 0
-  form.product.barcode = ''
+  form.name = ''
+  form.description = ''
 }
 
 function paginatorClick(e: PageState) {

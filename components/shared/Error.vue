95f3d468df2f93de967c723cde37fbc6e9a7affc
Hergen Roque Gamayon <gamayonhergenroque.web@gmail.com>
2023-12-20 13:04:54 +0800

    error

---
+++ components/shared/Error.vue
@@ -0,0 +1,16 @@
+<template>
+  <Message :severity="severity">{{ msg }}</Message>
+</template>
+
+<script lang="ts" setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  },
+  severity: {
+    type: String,
+    default: 'error'
+  }
+})
+</script>

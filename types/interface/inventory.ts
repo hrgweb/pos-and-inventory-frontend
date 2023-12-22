431ab822e94a4cc5c7f37de1aed7f87c2ac6ba0e
Hergen Roque Gamayon <gamayonhergenroque.web@gmail.com>
2023-12-23 02:26:40 +0800

    update inventory ts

--- types/interface/inventory.ts
+++ types/interface/inventory.ts
@@ -3,14 +3,15 @@ import { TransactionType } from '@/types/enum/transactionType'
 interface Product {
   name: string
   description: string
-  // category_id: number
+  category_id?: number
   // brand_id: number
-  supplier_id: number
+  supplier_id?: number
   cost_price: number
   selling_price: number
   stock_qty: number
   reorder_level: number
   barcode: string
+  is_available?: boolean
   created_at?: Date
 }
 

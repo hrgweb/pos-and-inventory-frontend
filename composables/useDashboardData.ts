35510006920dfa5ad6e20629d9b71418ddeaadb7
Hergen Roque Gamayon <gamayonhergenroque.web@gmail.com>
2023-12-25 21:26:01 +0800

    update composable data

--- composables/useDashboardData.ts
+++ composables/useDashboardData.ts
@@ -1,17 +1,29 @@
-import type { TransactionSession } from '@/types/interface/transactionSession';
-import type { Order } from '@/types/interface/order';
-import type { Supplier } from '@/types/interface/supplier';
+import type { TransactionSession } from '@/types/interface/transactionSession'
+import type { Order } from '@/types/interface/order'
+import type { Supplier } from '@/types/interface/supplier'
+import { usePageStore } from '@/store/page'
 
 type Data = {
-    transaction_session: TransactionSession
-    orders: Order[]
-    suppliers: Supplier[]
+  transaction_session: TransactionSession
+  orders: Order[]
+  suppliers: Supplier[]
 }
 
-export default async function (): Promise<Data | undefined> {
-    const config = useRuntimeConfig()
+export default async function (): Promise<void> {
+  const config = useRuntimeConfig()
+  const page = usePageStore()
 
-    return await $fetch(`${config.public.backendUrl}/api/data`, {
-        query: { transaction_session_no: localStorage.getItem('transaction_session_no') }
-    }) as Data
+  try {
+    const data = (await $fetch(`${config.public.backendUrl}/api/data`, {
+      query: {
+        transaction_session_no: localStorage.getItem('transaction_session_no')
+      }
+    })) as Data
+
+    page.transactionSession = data.transaction_session
+    page.orders = data?.orders
+    page.suppliers = data?.suppliers
+  } catch (error: any) {
+    console.log(error?.data)
+  }
 }

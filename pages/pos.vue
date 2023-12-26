c9757ee8ee041b484b70db666c02a7f19c480225
Hergen Roque Gamayon <gamayonhergenroque.web@gmail.com>
2023-12-26 14:31:25 +0800

    pos fixed and formatted

--- pages/pos.vue
+++ pages/pos.vue
@@ -2,7 +2,12 @@
   <div class="grid" style="height: 100vh">
     <div class="col-9" style="background-color: #f5f8fa">
       <div id="order-wrapper" style="height: 79vh">
-        <DataTable scrollable scrollHeight="79vh" :value="orders" tableClass="orders">
+        <DataTable
+          scrollable
+          scrollHeight="79vh"
+          :value="orders"
+          tableClass="orders"
+        >
           <Column field="product.name" header="Product"></Column>
           <Column field="product.selling_price" header="Price"></Column>
           <Column field="qty" header="Qty"></Column>
@@ -51,30 +56,58 @@
 
       <div class="actions relative" style="height: 100%">
         <Button label="New Transaction" @click="newTransaction" />
-        <Button label="Lookup" :disabled="!actionButtons.btnLookup" @click="openLookup" />
+        <Button
+          label="Lookup"
+          :disabled="!actionButtons.btnLookup"
+          @click="openLookup"
+        />
 
         <!-- Pay Now-->
-        <Button label="Pay" class="absolute" style="bottom: 0; left: 0" severity="info" :disabled="!actionButtons.btnPay"
-          @click="payment" />
+        <Button
+          label="Pay"
+          class="absolute"
+          style="bottom: 0; left: 0"
+          severity="info"
+          :disabled="!actionButtons.btnPay"
+          @click="payment"
+        />
       </div>
     </div>
 
-    <Dialog v-model:visible="showLookup" modal header="ITEM LOOKUP" :style="{ width: '60rem' }"
-      :breakpoints="{ '1199px': '75vw', '575px': '90vw' }" :dismissableMask="false" :draggable="false">
+    <Dialog
+      v-model:visible="showLookup"
+      modal
+      header="ITEM LOOKUP"
+      :style="{ width: '60rem' }"
+      :breakpoints="{ '1199px': '75vw', '575px': '90vw' }"
+      :dismissableMask="false"
+      :draggable="false"
+    >
       <form method="POST" @submit.prevent>
         <div class="flex flex-column gap-2">
           <span class="p-input-icon-left p-input-icon-right w-full">
             <i class="pi pi-search" />
-            <InputText type="text" v-model="searchByProductOrBarcode" class="w-full" id="search"
-              placeholder="Type product name or scan barcode" @input="findByProductOrBarcodeFn"
-              @keyup.enter="findViaEnter" />
+            <InputText
+              type="text"
+              v-model="searchByProductOrBarcode"
+              class="w-full"
+              id="search"
+              placeholder="Type product name or scan barcode"
+              @input="findByProductOrBarcodeFn"
+              @keyup.enter="findViaEnter"
+            />
             <i class="pi pi-qrcode" />
           </span>
         </div>
       </form>
       <br />
 
-      <DataTable :value="lookupItems" tableStyle="min-width: 50rem" :loading="isLookupLoading" stripedRows>
+      <DataTable
+        :value="lookupItems"
+        tableStyle="min-width: 50rem"
+        :loading="isLookupLoading"
+        stripedRows
+      >
         <template #empty>
           <p class="text-center">No results found</p>
         </template>
@@ -85,17 +118,34 @@
       </DataTable>
     </Dialog>
 
-    <Dialog v-model:visible="showPay" modal header="AMOUNT" :style="{ width: '35rem' }"
-      :breakpoints="{ '1199px': '75vw', '575px': '90vw' }" :dismissableMask="false" :draggable="false">
+    <Dialog
+      v-model:visible="showPay"
+      modal
+      header="AMOUNT"
+      :style="{ width: '35rem' }"
+      :breakpoints="{ '1199px': '75vw', '575px': '90vw' }"
+      :dismissableMask="false"
+      :draggable="false"
+    >
       <form method="POST" @submit.prevent>
-        <Message v-if="payError" class="mt-0" :closable="false" severity="warn">{{ payErrorMsg }}</Message>
+        <Message
+          v-if="payError"
+          class="mt-0"
+          :closable="false"
+          severity="warn"
+          >{{ payErrorMsg }}</Message
+        >
 
-        <InputText id="amount" class="text-3xl font-bold uppercase w-full" v-model.number="pay.amount"
-          @keyup.enter="paid" />
+        <InputText
+          id="amount"
+          class="text-3xl font-bold uppercase w-full"
+          v-model.number="pay.amount"
+          @keyup.enter="paid"
+        />
       </form>
     </Dialog>
 
-    <Toast position="top-left"/>
+    <Toast position="top-left" />
   </div>
 </template>
 
@@ -117,7 +167,7 @@ definePageMeta({
 })
 
 // const transactionSessionNo = ref('')
-let transasctionSession = reactive<TransactionSession>({
+let transactionSession = reactive<TransactionSession>({
   session_no: '',
   status: OrderStatus.PENDING
 })
@@ -155,10 +205,12 @@ onMounted(() => data())
 async function data(): Promise<void> {
   try {
     const data = (await $fetch(`${config.public.backendUrl}/api/data`, {
-      query: { transaction_session_no: localStorage.getItem('transaction_session_no') }
+      query: {
+        transaction_session_no: localStorage.getItem('transaction_session_no')
+      }
     })) as Data
 
-    transasctionSession = data.transaction_session
+    transactionSession = data.transaction_session
     orders.value = data?.orders
     suppliers.value = data?.suppliers
   } catch (error: any) {
@@ -202,13 +254,13 @@ async function findViaEnter(): Promise<void> {
     const qty = 1
     const subtotal = sellingPrice * qty
 
-    showLookup.value = false
+    console.log('called', config.public.backendUrl)
 
     try {
       const order = (await $fetch(`${config.public.backendUrl}/api/orders`, {
         method: 'POST',
         body: {
-          transaction_session_no: transasctionSession.session_no,
+          transaction_session_no: transactionSession.session_no,
           product: item,
           selling_price: sellingPrice,
           qty,
@@ -217,11 +269,13 @@ async function findViaEnter(): Promise<void> {
         }
       })) as Order
 
+      console.log('orders: ', orders)
+
+      showLookup.value = false
       orders.value.push(order)
     } catch (error: any) {
-      console.log(error?.data)
+      console.log('err: ', error)
     }
-    return
   }
 }
 
@@ -259,7 +313,12 @@ const isPaid = ref(false)
 
 function payment(): void {
   if (!orders.value.length) {
-    toast.add({ severity: 'warn', summary: 'No orders', detail: 'No orders for this transaction.', life: 3000 });
+    toast.add({
+      severity: 'warn',
+      summary: 'No orders',
+      detail: 'No orders for this transaction.',
+      life: 3000
+    })
     return
   }
 
@@ -278,7 +337,7 @@ async function paid(): Promise<void> {
     return
   }
 
-  sale.transaction_session_no = transasctionSession.session_no
+  sale.transaction_session_no = transactionSession.session_no
   sale.orders = orders.value
 
   try {
@@ -320,18 +379,23 @@ function toggleStateOfButtons(state: boolean): void {
 
 async function newTransaction(): Promise<void> {
   try {
-    const transactionSession = await $fetch(`${config.public.backendUrl}/api/transaction-sessions`, {
-      method: 'POST',
-    }) as TransactionSession
+    const session = (await $fetch(
+      `${config.public.backendUrl}/api/transaction-sessions`,
+      {
+        method: 'POST'
+      }
+    )) as TransactionSession
 
-    transasctionSession.session_no = transactionSession.session_no
-    transactionSession.status = transactionSession.status
+    transactionSession = session
 
-    localStorage.setItem('transaction_session_no', transactionSession.session_no)
+    localStorage.setItem(
+      'transaction_session_no',
+      transactionSession.session_no
+    )
 
-    data()  // fetch data
+    data() // fetch data
   } catch (error: any) {
-    console.log(error?.data);
+    console.log(error?.data)
   }
 }
 </script>

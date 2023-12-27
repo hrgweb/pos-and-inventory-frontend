31464265ede8b1c77dbfdfec48632a9e1d92ee0e
Hergen Roque Gamayon <gamayonhergenroque.web@gmail.com>
2023-12-28 00:22:26 +0800

    fetch inventory

--- store/inventory.ts
+++ store/inventory.ts
@@ -1,8 +1,66 @@
 import { defineStore } from 'pinia'
-import type { Inventory } from '@/types/interface/inventory'
+import type { Product, Inventory } from '@/types/interface/inventory'
+import { TransactionType } from '@/types/enum/transactionType'
+import { usePaginationStore } from '@/store/pagination'
+import type { Pagination } from '@/types/pagination'
 
 export const useInventoryStore = defineStore('inventory', () => {
-  const form = reactive<Inventory[]>([])
-  const formEdit = reactive<Inventory[]>([])
-  const contact = reactive<Inventory[]>([])
+  const pagination = usePaginationStore()
+
+  const form = reactive<Inventory>(formData())
+  const formEdit = reactive<Inventory>(formData())
+  const contact = reactive<Inventory>(formData())
+  const products = reactive<Product[]>([])
+  const loading = ref(false)
+  const search = ref('')
+  const list = ref<Inventory[] | null | undefined>([])
+
+  function formData() {
+    return {
+      product: {
+        id: null,
+        name: '',
+        description: '',
+        selling_price: 0,
+        stock_qty: 0,
+        reorder_level: 0,
+        barcode: ''
+      },
+      transaction_type: TransactionType.PURCHASE,
+      qty: 0,
+      cost_price: 0,
+      selling_price: 0,
+      subtotal: 0,
+      notes: ''
+    }
+  }
+
+  async function fetch(): Promise<Pagination | null | undefined> {
+    loading.value = true
+
+    try {
+      const paginate = (await $fetch<unknown>(
+        `${useBackendUrl()}/api/transactions`,
+        {
+          query: { page: pagination.curPage, search: search.value }
+        }
+      )) as Pagination
+
+      list.value = paginate?.data
+
+      return paginate
+    } catch (error) {
+      console.log(error)
+    } finally {
+      loading.value = false
+    }
+  }
+
+  return {
+    form,
+    formEdit,
+    contact,
+    products,
+    fetch
+  }
 })

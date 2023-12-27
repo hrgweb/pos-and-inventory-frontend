588cf440dd8a486c8fa29b071cfd3744d63344be
Hergen Roque Gamayon <gamayonhergenroque.web@gmail.com>
2023-12-28 00:50:57 +0800

    add function to store inventory

--- store/inventory.ts
+++ store/inventory.ts
@@ -3,6 +3,7 @@ import type { Product, Inventory } from '@/types/interface/inventory'
 import { TransactionType } from '@/types/enum/transactionType'
 import { usePaginationStore } from '@/store/pagination'
 import type { Pagination } from '@/types/pagination'
+import type { Errors } from '@/types/errors'
 
 export const useInventoryStore = defineStore('inventory', () => {
   const pagination = usePaginationStore()
@@ -14,6 +15,17 @@ export const useInventoryStore = defineStore('inventory', () => {
   const loading = ref(false)
   const search = ref('')
   const list = ref<Inventory[] | null | undefined>([])
+  const loadingForm = ref(false)
+  const showDialog = ref(false)
+  let err: Errors = reactive({
+    errors: {},
+    message: ''
+  })
+  const isAdd = ref(false)
+  const isEdit = ref(false)
+  const selectedIndex = ref(0)
+  const selectedProduct = ref<Product | null | undefined>()
+  const removing = ref(false)
 
   function formData() {
     return {
@@ -56,11 +68,102 @@ export const useInventoryStore = defineStore('inventory', () => {
     }
   }
 
+  async function save(): Promise<void> {
+    loadingForm.value = true
+
+    try {
+      const inventory = (await $fetch<unknown>(
+        `${useBackendUrl()}/api/transactions`,
+        {
+          method: 'POST',
+          body: contact
+        }
+      )) as Inventory
+
+      if (inventory) {
+        list.value?.unshift(inventory)
+        showDialog.value = false
+        reset()
+        resetError()
+      }
+    } catch (error: any) {
+      Object.assign(err, error?.data)
+    } finally {
+      loadingForm.value = false
+    }
+  }
+
+  async function removed(data: any, index: any): Promise<void> {
+    removing.value = true
+
+    try {
+      const deleted = (await $fetch<unknown>(
+        `${useBackendUrl()}/api/products/${data?.id}`,
+        { method: 'DELETE', body: { name: data?.name } }
+      )) as Product
+
+      if (deleted) {
+        if (list.value?.length) {
+          list.value.splice(index, 1)
+        }
+      }
+    } catch (error: any) {
+      Object.assign(err, error?.data)
+    } finally {
+      removing.value = false
+    }
+  }
+
+  function reset(): void {
+    // contact.selling_price = 0
+    // contact.name = ''
+    // contact.description = ''
+    // contact.selling_price = 0
+    // contact.stock_qty = 0
+    // contact.reorder_level = 0
+    // contact.barcode = ''
+  }
+
+  function resetError(): void {
+    err.errors = {}
+    err.message = ''
+  }
+
+  function add(): void {
+    showDialog.value = true
+    Object.assign(form, formData())
+    Object.assign(contact, form)
+    isAdd.value = true
+    isEdit.value = false
+  }
+
+  function edit(payload: any, index: number): void {
+    selectedIndex.value = index
+    selectedProduct.value = payload
+    showDialog.value = true
+    Object.assign(formEdit, payload)
+    Object.assign(contact, formEdit)
+    isAdd.value = false
+    isEdit.value = true
+  }
+
   return {
+    loadingForm,
+    isAdd,
+    isEdit,
+    showDialog,
+    loading,
+    search,
+    err,
+    list,
     form,
     formEdit,
     contact,
     products,
-    fetch
+    fetch,
+    save,
+    add,
+    edit,
+    removed
   }
 })

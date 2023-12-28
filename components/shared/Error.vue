e4d77d90b7d344636817f4d3b8ba73b19616ebbc
Hergen Roque Gamayon <gamayonhergenroque.web@gmail.com>
2023-12-28 16:29:03 +0800

    enhanced error component

--- components/shared/Error.vue
+++ components/shared/Error.vue
@@ -1,8 +1,12 @@
 <template>
-  <Message :severity="severity">{{ msg }}</Message>
+  <Message :severity="severity" @close="closed">{{ msg }}</Message>
 </template>
 
 <script lang="ts" setup>
+import { useInventoryStore } from '@/store/inventory'
+
+const inventory = useInventoryStore()
+
 defineProps({
   msg: {
     type: String,
@@ -13,4 +17,11 @@ defineProps({
     default: 'error'
   }
 })
+
+function closed() {
+  Object.assign(inventory.err, {
+    errors: {},
+    message: ''
+  })
+}
 </script>

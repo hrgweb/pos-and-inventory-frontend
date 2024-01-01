bdfa98219cb54d4f9534ab4274f240ac6b6d6c8d
Hergen Roque Gamayon <gamayonhergenroque.web@gmail.com>
2024-01-02 02:13:04 +0800

    create count occurence helper

--- utils/helper.ts
+++ utils/helper.ts
@@ -1,8 +1,37 @@
-// Function to count occurrences of a specific value in a property
-function countOccurrences(array: [], property: string, valueToCount: number) {
-  return array.reduce((count, obj) => {
-    return obj[property] === valueToCount ? count + 1 : count
-  }, 0)
+import type { Order } from '@/types/interface/order'
+
+function countOccurrences(orders: Order[]) {
+  type Result = {
+    id: number
+    name: string
+    count: number
+    [prop: string]: any
+  }
+
+  const newObj = [] as Result[]
+
+  for (let index = 0; index < orders.length; index++) {
+    const product = orders[index].product
+    const prevId =
+      index === 0 ? 0 : (orders[index - 1].product.id as number | undefined)
+    const currentId = orders[index].product.id
+
+    if (index > 0 && prevId === currentId) {
+      continue
+    }
+
+    const count = orders.filter(
+      (order: Order) => order.product['id'] === currentId
+    ).length
+
+    newObj.push({
+      id: Number(product['id']),
+      name: product['name'],
+      count: Number(count)
+    })
+  }
+
+  return newObj
 }
 
 export const util = {

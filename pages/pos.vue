cc2611c607bd2a0a1b8c2b0ac9d26b807af702cf
Hergen Roque Gamayon <gamayonhergenroque.web@gmail.com>
2024-01-05 14:55:09 +0800

    cleanup pos

--- pages/pos.vue
+++ pages/pos.vue
@@ -193,25 +193,16 @@
 </template>
 
 <script lang="ts" setup>
-import { ref, onMounted } from 'vue'
+import { onMounted } from 'vue'
 import { useDebounceFn } from '@vueuse/core'
-// import type { Product } from '@/types/interface/inventory'
-// import { OrderStatus, type Order } from '@/types/interface/order'
-// import type { Sale, SaleResult } from '@/types/interface/sale'
-// import type { TransactionSession } from '~/types/interface/transactionSession'
 import { usePageStore } from '@/store/page'
 import { usePosStore } from '@/store/pos'
-// import { util } from '@/utils/helper'
 
 const page = usePageStore()
 const pos = usePosStore()
 const keyShortcut = useKeyboardShortcuts()
 
 definePageMeta({ layout: false, middleware: 'sanctum:auth' })
-
-// const searchByProductOrBarcode = ref('')
-// const isLookupLoading = ref(false)
-// const lookupItems = ref<Product[]>([])
 
 watch(
   [() => page.orders, () => page.transactionSession],
@@ -253,66 +244,6 @@ const findByProductOrBarcodeFn = useDebounceFn(
   500
 )
 
-// const findByProductOrBarcodeFn = useDebounceFn(async () => {
-//   if (searchByProductOrBarcode.value.length <= 0) {
-//     return
-//   }
-
-//   // reset errors
-//   payError.value = false
-//   payErrorMsg.value = ''
-
-//   isLookupLoading.value = true
-
-//   try {
-//     const products = (await $fetch(`${useBackendUrl()}/api/products/lookup`, {
-//       query: {
-//         search: searchByProductOrBarcode.value
-//       }
-//     })) as Product[]
-
-//     lookupItems.value = products
-//   } catch (error: any) {
-//     console.log(error.data)
-//   }
-
-//   isLookupLoading.value = false
-// }, 500)
-
-// async function findViaEnter(): Promise<void> {
-//   // no products found
-//   if (lookupItems.value.length <= 0) return
-
-//   if (lookupItems.value.length === 1) {
-//     let item = lookupItems.value[0] as Product
-
-//     const sellingPrice = item?.selling_price
-//     const qty = 1
-//     const subtotal = sellingPrice * qty
-
-//     try {
-//       const order = (await $fetch(`${useBackendUrl()}/api/orders`, {
-//         method: 'POST',
-//         body: {
-//           transaction_session_no: page.transactionSession?.session_no,
-//           product: item,
-//           selling_price: sellingPrice,
-//           qty,
-//           subtotal,
-//           status: OrderStatus.PENDING
-//         }
-//       })) as Order
-
-//       pos.showLookup = false
-//       page.orders.push(order)
-//     } catch (error: any) {
-//       console.log('err: ', error?.data)
-//       payError.value = true
-//       payErrorMsg.value = error?.data?.message
-//     }
-//   }
-// }
-
 function grandTotal(): number {
   if (page.orders.length > 0) {
     return page.orders.reduce((acc, order) => {
@@ -334,99 +265,6 @@ function scrollToBottom() {
     scrollingContainer.scrollTop = scrollingContainer.scrollHeight
   }
 }
-
-// const payError = ref(false)
-// const payErrorMsg = ref('')
-// const isPaid = ref(false)
-
-// const sale = reactive<Sale>({
-//   transaction_session_no: '',
-//   orders: [],
-//   grand_total: 0,
-//   amount: 0,
-//   product_count_occurences: []
-// })
-
-// async function paid(): Promise<void> {
-//   if (page.pay.amount < page.pay.grandTotal) {
-//     payError.value = true
-//     payErrorMsg.value = 'Amount must be greater than the total.'
-//     return
-//   }
-
-//   sale.transaction_session_no = page.transactionSession?.session_no
-//   sale.orders = page.orders
-//   sale.grand_total = page.pay.grandTotal
-//   sale.amount = page.pay.amount
-//   sale.change = page.pay.amount - page.pay.grandTotal
-//   sale.product_count_occurences = util.countOccurrences(page.orders)
-
-//   try {
-//     const payment = (await $fetch(`${useBackendUrl()}/api/sales`, {
-//       method: 'POST',
-//       body: sale
-//     })) as SaleResult
-
-//     if (payment && payment.success) {
-//       payError.value = false
-//       payErrorMsg.value = ''
-//       page.pay.change = page.pay.amount - page.pay.grandTotal
-//       pos.showPay = false
-//       isPaid.value = true
-//       // page.orders = []
-//       pos.toggleStateOfButtons(true)
-//       pos.actionButtons.btnTransaction = false
-//       pos.blocked = true
-//     }
-//   } catch (error: any) {
-//     console.log(error?.data)
-//   }
-// }
-
-// type Buttons = {
-//   btnTransaction: boolean
-//   btnLookup: boolean
-//   btnPay: boolean
-//   btnVoid: boolean
-// }
-
-// let actionButtons = ref<Buttons>({
-//   btnTransaction: true,
-//   btnLookup: true,
-//   btnPay: true,
-//   btnVoid: true
-// })
-
-// function toggleStateOfButtons(state: boolean): void {
-//   actionButtons.value.btnTransaction = state
-//   actionButtons.value.btnLookup = state
-//   actionButtons.value.btnPay = state
-//   actionButtons.value.btnVoid = state
-// }
-
-// async function newTransaction(): Promise<void> {
-//   try {
-//     const session = (await $fetch(
-//       `${useBackendUrl()}/api/transaction-sessions`,
-//       {
-//         method: 'POST'
-//       }
-//     )) as TransactionSession
-
-//     page.transactionSession = session
-//     pos.blocked = false
-
-//     localStorage.setItem(
-//       'transaction_session_no',
-//       page.transactionSession?.session_no
-//     )
-
-//     useDashboardData() // fetch data
-//     toggleStateOfButtons(false)
-//   } catch (error: any) {
-//     console.log(error?.data)
-//   }
-// }
 
 const { logout } = useSanctumAuth()
 
